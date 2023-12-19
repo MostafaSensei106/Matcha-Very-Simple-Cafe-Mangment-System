c0bac05a78820507b22f92708c7e17042602def8
wwwmo <wwwmostafagmer100@gmail.com>
2023-12-19 23:03:35 +0200

    Matcha_Cafe v 0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
 
+public class Main {
+
+    public  static  void  login_page (){
+        UI_Login login = new UI_Login();
+        login.setContentPane(login.Login_panal);
+        login.setTitle("Matcha Cafe - Login");
+        int w ;
+        int h ;
+        w =800;
+        h=800;
+        login.setSize(new Dimension(w, h));
+        login.setVisible(true);
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
+
+    public static void main(String[] args) {
+     login_page();
+    }
+
 }
